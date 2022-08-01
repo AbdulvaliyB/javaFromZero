8f0eede82de26fa65f5f8d8478d43ab6d7c270d9
AbdulvaliyB <97952892+AbdulvaliyB@users.noreply.github.com>
2022-08-01 17:07:30 +0500

    commit

--- src/main/java/Four/four/homeWork/two/Main.java
+++ src/main/java/Four/four/homeWork/two/Main.java
@@ -1,7 +1,52 @@
 package Four.four.homeWork.two;
 
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Set<Student> studentHashSet = new HashSet<>();
+
+        System.out.println("You are creating new studentHashSet! ");
+
+        while (true) {
+            System.out.print("Enter the full name: ");
+            String fullName = scanner.nextLine();
+            if ("end".equals(fullName)) {
+                break;
+            }
+            System.out.print("Enter the group: ");
+            String group = scanner.nextLine();
+            if ("end".equals(group)) {
+                break;
+            }
+            System.out.print("Enter the student id: ");
+            String studentId = scanner.nextLine();
+            if ("end".equals(studentId)) {
+                break;
+            }
+            boolean check = true;
+            for (Student student : studentHashSet) {
+                if (student.getStudentId().equals(studentId)) {
+                    System.out.println("We have this student");
+                    check = false;
+                }
+            }
+            if (!check) continue;
+            Student student = new Student(fullName, group, studentId);
+            studentHashSet.add(student);
+            System.out.println("To finish enter \"end\"");
+        }
+
+        System.out.println("List of students: ");
+        int i = 1;
+        for (Student student : studentHashSet) {
+            System.out.println(i + "." + student);
+            i++;
+        }
 
     }
 }
--- src/main/java/Four/four/homeWork/two/Student.java
+++ src/main/java/Four/four/homeWork/two/Student.java
@@ -1,16 +1,13 @@
 package Four.four.homeWork.two;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @AllArgsConstructor
 @Getter
 @Setter
 @ToString
 public class Student {
-    private String name;
+    private String fullName;
     private String group;
     private final String studentId;
 
